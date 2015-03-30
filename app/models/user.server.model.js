9a4195a272147604f59196e00ba0f139bc1c39e4
Eric Ihli <ericihli@gmail.com>
2015-03-30 08:50:06 -0500

    Add passport strategy

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,16 +4,42 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
+  email: {
+    type: String,
+    index: true,
+    match: /.+\@.+\..+/
+  },
   username: {
     type: String,
     trim: true,
-    unique: true
+    unique: true,
+    required: true
   },
-  password: String,
+  password: {
+    type: String,
+    validate: [
+      function(password) {
+        return password.length >= 6;
+      },
+      'Password should be longer'
+    ],
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Owner', 'User']
+  },
   created: {
     type: Date,
     default: Date.now
+  }
+});
+
+UserSchema.post('save', function(next) {
+  if (this.isNew) {
+    console.log('A new user was created.');
+  } else {
+    console.log('A user updated its details');
   }
 });
 
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
   conf = require('./config/config'),
-  express = require('./config/express');
+  express = require('./config/express'),
+  passport = require('./config/passport');
 
 var db = mongoose();
-
 var app = express();
+var passport = passport();
+
 app.listen(conf.port);
 module.exports = app;
 
