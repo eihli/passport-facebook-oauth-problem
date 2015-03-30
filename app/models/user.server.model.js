11ecb9aa3fa358a6f895181e7a5631d87ca9bb93
Eric Ihli <ericihli@gmail.com>
2015-03-30 00:28:52 -0500

    Add virtual fullname property to user.
    On Model validation chapter of MEAN Web Development.
    Learning Mongoose/mongodb.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,12 +5,24 @@ var UserSchema = new Schema({
   firstName: String,
   lastName: String,
   email: String,
-  username: String,
+  username: {
+    type: String,
+    trim: true,
+    unique: true
+  },
   password: String,
   created: {
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
 });
 
 mongoose.model('User', UserSchema);
