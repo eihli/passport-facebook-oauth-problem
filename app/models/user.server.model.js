7c0a1f77294a2f00ecac0755dc33e07c0a98be15
Eric Ihli <ericihli@gmail.com>
2015-03-31 10:34:05 -0500

    Add methods for passport and authentication.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+  crypto = require('crypto'),
   Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -6,33 +7,76 @@ var UserSchema = new Schema({
   lastName: String,
   email: {
     type: String,
-    index: true,
-    match: /.+\@.+\..+/
+    match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
   },
   username: {
     type: String,
     trim: true,
     unique: true,
-    required: true
+    required: 'Username is required'
   },
   password: {
     type: String,
     validate: [
       function(password) {
-        return password.length >= 6;
-      },
-      'Password should be longer'
+        return password && password.length >= 6;
+      }, 'Password should be longer'
     ],
-    required: true
   },
-  role: {
+  salt: {
+    type: String
+  },
+  provider: {
     type: String,
-    enum: ['Admin', 'Owner', 'User']
+    required: 'Provider is required'
   },
+  providerId: String,
+  providerData: {},
   created: {
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+
+    next();
+  };
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      } 
+    } else {
+      callback(null);
+    }
+  });
+};
+
+UserSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
 });
 
 UserSchema.post('save', function(next) {
