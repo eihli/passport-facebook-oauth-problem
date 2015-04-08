2fb6454698897990be2b100572d70422a55e9291
Eric Ihli <ericihli@gmail.com>
2015-04-08 00:03:45 -0500

    Finish OAuth local implementation.

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,6 @@
 exports.render = function(req, res) {
-  if (req.session.lastVisit) {
-    console.log(req.session.lastVisit);
-  }
-
-  req.session.lastVisit = new Date();
-
   res.render('index', {
-    title: 'Hello to all the World!'
+    title: 'Hello to all the World!',
+    userFullName: req.user? req.user.fullName : ''
   });
 };
